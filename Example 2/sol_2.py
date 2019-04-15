4266824dc22896da5d0a49ba60c04aedc8625bfc
Paul Louis <iampaullouis@gmail.com>
2019-04-15 13:53:43 +0530

    Add square rectangle problem with sols

---
+++ Example 2/sol_2.py
@@ -0,0 +1,60 @@
+# Add another layer of abstraction to “simulate” interface type (solve it by inheritance too but in a different way).
+
+class Shape:
+	def calculate_area(self):
+		raise NotImplementedError('This is not implemented in the base class')
+
+class Rectangle(Shape):
+	def __init__(self):
+		self._height = 0
+		self._width = 0
+
+	@property
+	def height(self):
+		return self._height
+	
+	@property
+	def width(self):
+		return self._width
+	
+	@height.setter	
+	def set_height(self, height):
+		self._height = height
+
+	@width.setter
+	def set_width(self, width):
+		self._width = width
+
+	def calculate_area(self):
+		return self._height*self._width
+
+class Square(Rectangle):
+	def __init__(self):
+		self._side = 0
+
+	@property
+	def side(self):
+		return self._side
+
+	@side.setter
+	def set_side(self, side):
+		self._side = side
+	
+	def calculate_area(self):
+		return pow(self._side, 2)
+
+def main():
+	rectangle = Rectangle()
+	rectangle.set_height = 5
+	rectangle.set_width = 10
+	print(rectangle.calculate_area())
+
+	square = Square()
+	square.set_side = 5
+	print(square.calculate_area())
+
+	shape = Shape()
+	return shape.calculate_area()
+
+if __name__ == '__main__':
+	main()
---
+++ Example 2/square_rectangle.py
@@ -0,0 +1,61 @@
+# bad code :(
+
+class Rectangle:
+	def __init__(self):
+		self._height = 0
+		self._width = 0
+
+	@property
+	def height(self):
+		return self._height
+	
+	@property
+	def width(self):
+		return self._width
+	
+	@height.setter	
+	def set_height(self, height):
+		self._height = height
+
+	@width.setter
+	def set_width(self, width):
+		self._width = width
+
+	def calculate_area(self):
+		return self._height*self._width
+
+class Square(Rectangle):
+	@property
+	def height(self):
+		return self._height
+	
+	@property
+	def width(self):
+		return self._width
+		
+	@height.setter
+	def set_height(self, height):
+		self._height = height
+		self._width = height
+	
+	@width.setter
+	def set_width(self, width):
+		self._width = width
+		self._height = width
+
+	def calculate_area(self):
+		return pow(self._height,2)
+
+def main():
+	rectangle = Rectangle()
+	rectangle.set_height = 5
+	rectangle.set_width = 10
+	print(rectangle.calculate_area())
+	square = Square()
+	square.set_width = 5
+	square.set_height = 10
+	print(square.calculate_area())
+
+
+if __name__ == '__main__':
+	main()
